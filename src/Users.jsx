ab6ab5651d02b4475007103d6de74edffad5a01c
Sadek Ali <sadekali1530@gmail.com>
2023-10-17 22:23:35 +0600

    add the code

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,19 +1,27 @@
 
 import { useLoaderData } from 'react-router-dom';
 
-const handlremove=()=>{
-
+const handlremove = (id) => {
+    console.log(id)
+    fetch(`http://localhost:5000/users/${id}`,{
+        method : 'DELETE'
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        alert('successfully delete the data')
+        console.log(data)
+    })
 }
 
 const Users = () => {
-    const data= useLoaderData()
+    const data = useLoaderData()
     return (
         <div>
             <h1> this is user page</h1>
             <h1>{data.length}</h1>
             <div>
                 {
-                    data.map(data=><p key={data._id}>{data.name} : {data.email} <button onClick={handlremove}>X</button>  </p>)
+                    data.map(data => <p key={data._id}>{data.name} : {data.email} <button onClick={() => handlremove(data._id)}>X</button>  </p>)
                 }
             </div>
         </div>
